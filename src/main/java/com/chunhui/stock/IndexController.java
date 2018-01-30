66ff79d96e564d67e267a6afbae9301af2c17e36
guchunhui.gch <guchunhui.gch@alibaba-inc.com>
2018-01-30 19:41:23 +0800

    add init project

---
+++ src/main/java/com/chunhui/stock/IndexController.java
@@ -0,0 +1,14 @@
+package com.chunhui.stock;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @RequestMapping("/")
+    public String info(){
+        return "Stock Index";
+    }
+
+}
